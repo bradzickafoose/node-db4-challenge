cfe6d57b9618aad6a1a730e176f79eb46e00c62c
Brad <brad@viziworx.com>
2019-12-12 18:39:57 -0500

    setup schema

--- data/migrations/20191212174902_recipe_tables.js
+++ data/migrations/20191212174902_recipe_tables.js
@@ -1,8 +1,62 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('recipes', tbl => {
+            tbl.increments();
+
+            tbl.spring('name', 255)
+                .notNullable()
+                .unique();
+        })
+
+        .createTable('ingredients', tbl => {
+            tbl.increments();
+
+            tbl.string('name', 255)
+                .notNullable();
+
+            // Foreign Key
+            tbl.integer('ingredients_id')
+                .unsigned()
+                .notNullable()
+                .references('id') // ?
+                .inTable(''); // ?
+        })
+
+        .createTable('instructions', tbl => {
+            tbl.increments();
+
+            tbl.integer('instructions_id')
+                .unsigned()
+                .notNullable();
+
+            tbl.integer('step_number')
+                .notNullable();
+
+            tbl.string('instructions')
+                .notNullable();
+
+        })
+
+        .createTable('recipe_ingredients', tbl => {
+            tbl.primary({ 'recipe_id', 'ingredient_id'});
+
+            tbl.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('recipe');
+
+            tbl.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('ingredients');
+        });
+
+
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
